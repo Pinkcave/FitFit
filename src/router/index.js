d1e7a0ec383072a7c90e7ff006a6a0f5b169c8c2
Pinkcave <104346568+Pinkcave@users.noreply.github.com>
2024-05-14 21:13:05 +0800

    update login signup

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from "@/views/LoginView.vue";
+import SignUpView from "@/views/SignUpView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,7 +18,22 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    }
+    },
+    //登录页面
+    {
+      // path:'/login',
+      path:"/",
+      name:'LogInView',
+      component:LoginView,
+      meta: { requiresAuth: false }
+    },
+    //注册页面
+    {
+      path:'/SignUp',
+      name:'SignUpView',
+      component:SignUpView,
+      meta: { requiresAuth: false }
+    },
   ]
 })
 
