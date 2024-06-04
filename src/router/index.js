e9e387c634d2317fa0f60b7920cea0dfc140965f
pzone <pzone@noreply.gitcode.com>
2024-06-04 21:01:02 +0800

    zpz
    course and activity view

--- src/router/index.js
+++ src/router/index.js
@@ -2,27 +2,22 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from "@/views/LoginView.vue";
 import SignUpView from "@/views/SignUpView.vue";
+import CourseView from '@/views/CourseView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    //课程报名
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path:"/",
+      name:'course',
+      component:CourseView,
+      meta: { requiresAuth: false }
     },
     //登录页面
     {
       // path:'/login',
-      path:"/",
+      path:"/login",
       name:'LogInView',
       component:LoginView,
       meta: { requiresAuth: false }
